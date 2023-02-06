507768db3e63dc23fa98bc45316b6ea81aeb60ce
armanKamali <arman.amirkamali@gmail.com>
2023-02-06 11:32:10 +0300

    modified:   admin 	modified:   server/app/Http/Controllers/Admin/AdminController.php 	new file:   server/app/Http/Controllers/Admin/IMG_0651.JPG 	renamed:    server/app/Http/Middleware/AdminToken.php -> server/app/Http/Controllers/Middleware/AdminToken.php 	renamed:    server/app/Http/Middleware/Authenticate.php -> server/app/Http/Controllers/Middleware/Authenticate.php 	renamed:    server/app/Http/Middleware/EncryptCookies.php -> server/app/Http/Controllers/Middleware/EncryptCookies.php 	renamed:    server/app/Http/Middleware/PreventRequestsDuringMaintenance.php -> server/app/Http/Controllers/Middleware/PreventRequestsDuringMaintenance.php 	renamed:    server/app/Http/Middleware/RedirectIfAuthenticated.php -> server/app/Http/Controllers/Middleware/RedirectIfAuthenticated.php 	renamed:    server/app/Http/Middleware/TrimStrings.php -> server/app/Http/Controllers/Middleware/TrimStrings.php 	renamed:    server/app/Http/Middleware/TrustHosts.php -> server/app/Http/Controllers/Middleware/TrustHosts.php 	renamed:    server/app/Http/Middleware/TrustProxies.php -> server/app/Http/Controllers/Middleware/TrustProxies.php 	renamed:    server/app/Http/Middleware/VerifyCsrfToken.php -> server/app/Http/Controllers/Middleware/VerifyCsrfToken.php

--- server/app/Http/Controllers/Admin/AdminController.php
+++ server/app/Http/Controllers/Admin/AdminController.php
@@ -4,25 +4,44 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Glass;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    
-    public function getGlasses(){
+
+    public function getGlasses()
+    {
         return Glass::all();
     }
-    
 
-    public function changePhoto(Request $request){
+
+    public function changePhoto(Request $request)
+    {
         $id = $request->input('id');
+        $product_id = $request->input('product_id');
+        $path =  base_path('../public_html/product-photo/');
+        $product = Product::find($product_id);
 
+        if ($id == 0) {
+            if (glob($path . $product->photo))
+                unlink($path . $product->photo);
+
+            if ($request->hasFile('file')) {
+                $image = $request->file('file');
+                $file_name = $request->file('file')->getClientOriginalName();
+                $product->photo = $file_name;
+                $product->save();
+                $image->move($path, $file_name);
+                return $file_name;
+            }
+        }
+        return $id;
         if ($request->hasFile('file')) {
             $image = $request->file('file');
             $destinationPath = base_path('../public_html/product-photo');
             $file_name = $request->file('file')->getClientOriginalName();;
             $image->move($destinationPath, $file_name);
         }
-
     }
 }
